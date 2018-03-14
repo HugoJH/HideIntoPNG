37d0f440426103696aa26c5a47252250e6959b5d
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-03-14 15:55:35 +0100

    Encryption workflow green test; some cosmetic changes

--- Tests/test_encrypter.py
+++ Tests/test_encrypter.py
@@ -4,18 +4,11 @@ from shutil import rmtree
 from ..encrypter import Encrypter
 
 def test_encryption_workflow():
-	_resetResultsFolder()
 	mockClearFileFilePath = join(dirname(__file__), "mock/ninja.png")
-	mockEncryptedFileFilePath = join(dirname(__file__), "mock/ninja.encrypted")
 	enc = Encrypter()
 
 	with open(mockClearFileFilePath, "rb") as mockClearFD:
 		data = mockClearFD.read()
 		encryptedData = enc.encryptData(data, "ILoveRamen")
-		decryptedData = enc.decryptData(encryptedData,"ILoveRamen")
+		decryptedData = enc.decryptData(encryptedData, "ILoveRamen")
 		assert data == decryptedData
-
-def _resetResultsFolder():
-    if exists(join(dirname(__file__), "results")):
-        rmtree(join(dirname(__file__), "results"))
-    makedirs(join(dirname(__file__), "results"))
--- encrypter.py
+++ encrypter.py
@@ -1,7 +1,45 @@
+import Crypto.Random
+from Crypto.Cipher import AES
+import hashlib
+
 class Encrypter:
 
-    def encryptData(self, data, passphrase):
-        pass
+    AES_ALIGNMENT = 16
+    ITERATIONS = 42
+    SALT_SIZE = 16
 
-    def decryptData(self, data, passphrase):
-        pass
+    def encryptData(self, data, passphrase):
+        salt = Crypto.Random.get_random_bytes(self.SALT_SIZE)
+
+        key = self._generateKey(bytearray(passphrase.encode('utf-8')) + bytearray(salt), self.ITERATIONS)
+        cipher = AES.new(key, AES.MODE_ECB)
+        paddedClearData = self._padData(data, self.AES_ALIGNMENT)
+        encryptedData = cipher.encrypt(paddedClearData)
+
+        return salt + encryptedData
+
+    def decryptData(self, encryptedData, passphrase):
+        salt = encryptedData[0:self.SALT_SIZE]
+        unsaltedEncryptedData = encryptedData[self.SALT_SIZE:]
+        key = self._generateKey(bytearray(passphrase.encode('utf-8')) + bytearray(salt), self.ITERATIONS)
+        cipher = AES.new(key, AES.MODE_ECB)
+
+        return self._unpadData(cipher.decrypt(unsaltedEncryptedData))
+
+
+    def _padData(self, data, alignment):
+        padding_size = alignment - len(data) % alignment
+        padding = bytes([padding_size]) * padding_size
+        return data + padding
+
+    def _unpadData(self, paddedData):
+        paddingSize = paddedData[-1]
+        return paddedData[:-paddingSize]
+
+    def _generateKey(self, saltedPassPhrase, iterations):
+        assert iterations > 0
+
+        for i in range(iterations):
+            saltedPassPhrase = hashlib.sha256(saltedPassPhrase).digest()
+
+        return saltedPassPhrase
