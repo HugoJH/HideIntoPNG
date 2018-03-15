99888ca7496b08c54865ad883f1eabf2c95e48d8
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-03-15 12:57:47 +0100

    HideIntoPNG Workflow green test

--- Tests/test_hideintopng.py
+++ Tests/test_hideintopng.py
@@ -1,6 +1,7 @@
 from ..hideintopng import HideIntoPNG
 
-from os.path import exists, join, dirname
+from os.path import exists, join, dirname, basename
+
 
 def test_hide_into_png_workflow():
 
@@ -8,6 +9,12 @@ def test_hide_into_png_workflow():
     containerPNGFilePath = join(dirname(__file__), "mock/ninja.png")
     mockResultFilePath = join(dirname(__file__), "mock/ninjaThatAteTheRamen.png")
     hip = HideIntoPNG()
-    result = hip.hide(containerPNGFilePath, payloadFilePath)
-    with open (mockResultFilePath, "rb") as mockFD:
-        assert(result == mockFD.read())
+    hiddenPayload = b''
+    with open(containerPNGFilePath, "rb") as containerFD:
+        with open(payloadFilePath, "rb") as payloadFD:
+            hiddenPayload = hip.hide(containerFD.read(), basename(payloadFilePath).encode('utf-8'), payloadFD.read(), "ILoveRamen")
+    extractedPayload = hip.extract(hiddenPayload, "ILoveRamen")
+
+    with open (payloadFilePath, "rb") as mockFD:
+        assert(extractedPayload['data'] == mockFD.read())
+    assert(extractedPayload['meta'] == basename(payloadFilePath).encode('utf-8'))
--- hideintopng.py
+++ hideintopng.py
@@ -3,5 +3,27 @@ from .encrypter import Encrypter
 
 class HideIntoPNG():
 
-	def hide(self, containerPNGFilePath, payloadFilePath):
-		pass
+	def hide(self, containerData, payloadMetaData, payloadData, passPhrase):
+		emb = Embedder()
+		enc = Encrypter()
+
+		encryptedPayloadMeta = enc.encryptData(payloadMetaData, passPhrase)
+		encryptedPayloadData = enc.encryptData(payloadData, passPhrase)
+
+		return emb.insertPayload(containerData, encryptedPayloadMeta, encryptedPayloadData)
+
+	def extract(self, containerWithPayloadData, passPhrase):
+		emb = Embedder()
+		enc = Encrypter()
+
+		containerData = b''
+		encryptedPayload = emb.extractPayload(containerWithPayloadData)
+
+		payload = {}
+		payload['meta'] = enc.decryptData(encryptedPayload['filename'], passPhrase)
+		payload['data'] = enc.decryptData(encryptedPayload['data'], passPhrase)
+
+
+		return payload
+
+
