2d2e057800214bb8f8f3765dbfb24dea816c4e87
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2018-03-15 14:09:52 +0100

    Added docstring for hide and extract methods

--- hideintopng.py
+++ hideintopng.py
@@ -4,6 +4,20 @@ from .encrypter import Encrypter
 class HideIntoPNG():
 
 	def hide(self, containerData, payloadMetaData, payloadData, passPhrase):
+		"""
+			Embeds data and metadata both encrypted with a passPhrase in a PNG container.
+
+			Parameters:
+				containerData (bin) -- Binary Data of a sample PNG file
+				payloadMetaData (bin) -- Metadata to be encrypted and embeded in the ContainerData
+				payloadMetaData (bin) -- data to be encrypted and embeded in the ContainerData
+				passPhrase (str) -- password used to encrypt data and metadata before embed both in the ContainerData
+
+			Returns:
+				bin: ContainerData with Data and Metadata embedded within.
+
+		"""
+
 		emb = Embedder()
 		enc = Encrypter()
 
@@ -13,6 +27,17 @@ class HideIntoPNG():
 		return emb.insertPayload(containerData, encryptedPayloadMeta, encryptedPayloadData)
 
 	def extract(self, containerWithPayloadData, passPhrase):
+		"""
+			Extracts data and metadata both encrypted with a passPhrase from a PNG container's data.
+
+			Parameters:
+				containerWithPayloadData (bin) -- PNG binary data with the payload embedded within.
+				passPhrase (str) -- password used to decrypt data and metadata embedded in the containerWithPayloadData.
+
+			Returns:
+				dict: Data and Metadata embedded in the containerWithPayloadData.
+		"""
+
 		emb = Embedder()
 		enc = Encrypter()
 
@@ -25,5 +50,3 @@ class HideIntoPNG():
 
 
 		return payload
-
-
